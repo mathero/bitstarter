cfd4077b795731af9f44f620c6abe9df4e2943cc
Owen Mather <opm24@btinternet.com>
2013-07-09 21:32:46 +0000

    index

--- web.js
+++ web.js
@@ -1,10 +1,21 @@
 var express = require('express');
-
+var fs = require('fs');
+var buffer = new Buffer();
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+  buffer.write(data, "utf-8")
+  var content = buffer.toString('utf-8');
 });
+
+
+app.get('/', function(request, response) {
+  response.send(content);
+});
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
